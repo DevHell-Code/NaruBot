99f3b42d5fecf92f7a454f7021cc703e92ee0488
KeySpace <77488170+froggal@users.noreply.github.com>
2022-07-12 06:06:44 +0000

    백준 솔브닥 구하기 추가.

--- bot/Boj.py
+++ bot/Boj.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 import random
 from baekjoon import boj
+from baekjoon import solvedac
 
+# https://github.com/smartwe/baekjoon-api
 
 # 임배드 함수
 def embed(title, description, color=random.randint(0x000000, 0xFFFFFF)):
@@ -17,9 +19,12 @@ class Boj(commands.Cog):
     async def 백준(self, ctx, name):
         await ctx.reply(embed=embed(
             'Boj Profile',
-            f'아이디 {boj.get_status_message(name)} \n 랭크 : {boj.get_rank(name)} \n 맞은 문제들 : {boj.get_correct_qs(name)} \n 맞은 문제들의 개수 : {boj.get_correct_q(name)} \n 시도했지만 맞지 않은 문제들 : {boj.get_unsolved_qs(name)} \n 시도했지만 맞지 않은 문제의 개수 : {boj.get_unsolved_q(name)} \n 제출 개수 : {boj.get_submit_time(name)}'
+            f'소개: {boj.get_status_message(name)} \n 랭크 : {boj.get_rank(name)} \n 맞은 문제들 : {boj.get_correct_qs(name)} \n 맞은 문제들의 개수 : {boj.get_correct_q(name)} \n 시도했지만 맞지 않은 문제들 : {boj.get_unsolved_qs(name)} \n 시도했지만 맞지 않은 문제의 개수 : {boj.get_unsolved_q(name)} \n 제출 개수 : {boj.get_submit_time(name)}'
         ))
 
+    @commands.command()
+    async def 솔브닥(self, ctx, name):
+      await ctx.reply(embed=embed('Solved.ac Profile', f'티어: {solvedac.get_tier(name)} \n AC RATING: {solvedac.get_ac_rating(name)} \n exp: {solvedac.get_exp(name)} \n 랭크: {solvedac.get_rank(name)}'))
 
 def setup(bot):
     bot.add_cog(Boj(bot))
